33a056be68e9325bc526b9fb5f79c0a415211e12
Audrey <audrey.gillies@gmail.com>
2021-10-05 15:19:50 -0600

    typo for pushing a new book in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -52,7 +52,7 @@ const resolvers = {
       if (context.user) {
         const userUpdate = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $apush: { savedBooks: bookData } },
+          { $push: { savedBooks: bookData } },
           { new: true, runValidators: true }
         );
 
